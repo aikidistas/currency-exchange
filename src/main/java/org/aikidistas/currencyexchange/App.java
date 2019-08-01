5cb298e4a44fca2306a5aca8b26ce244858762db
Nerijus Vaiciulis <nerijus.vaiciulis@danskebank.lt>
2019-08-01 08:28:03 +0300

    Initial maven project commit

---
+++ src/main/java/org/aikidistas/currencyexchange/App.java
@@ -0,0 +1,13 @@
+package org.aikidistas.currencyexchange;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
